f9cda7e06afc9272e6e0a33b27c67faf3652bed7
hunter-graves <server.hunter@gmail.com>
2018-03-08 23:36:05 -0800

    holy shit

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -46,7 +46,7 @@ void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* 
     static double a[BLOCK_SIZE*BLOCK_SIZE] __attribute__ ((aligned (16)));
     //static double b[BLOCK_SIZE*BLOCK_SIZE] __attribute__ ((aligned (16)));
 
-    static double temp[2] __attribute__ ((aligned (16)));
+
 
     __m128d vecA;
     __m128d vecB;
@@ -76,6 +76,7 @@ void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* 
 /* Compute C(i,j) */
             double cij = C[i + j * lda];
             double tmpor = 0;
+            static double temp[2] __attribute__ ((aligned (16)));
 
             for (int k = 0; k < K; k += 4) {
                 vecA = _mm_load_pd(&a[k + i * BLOCK_SIZE]);
