db2428ba893f020457c5ae3093135f9fee111dda
hunter-graves <server.hunter@gmail.com>
2018-03-09 06:00:56 -0800

    32block sweet spot

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -17,7 +17,7 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #include <emmintrin.h>
 #include <immintrin.h>
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 32
+#define BLOCK_SIZE 64
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
