5c52e58e658457cb840fba94ca266b7eedc61203
hunter-graves <server.hunter@gmail.com>
2018-03-09 03:54:15 -0800

    256bit SIMD instructions corrections

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -15,7 +15,7 @@ LDLIBS = -lrt -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_intel_lp64.a $(MKL
 
 const char* dgemm_desc = "Simple blocked dgemm.";
 #include <emmintrin.h>
-#include  <immintrin.h
+#include <immintrin.h>
 #if !defined(BLOCK_SIZE)
 #define BLOCK_SIZE 64
 #endif
