23a959ec2d19e254a188dee0c1602bc21ac20eca
hunter-graves <server.hunter@gmail.com>
2018-03-09 09:58:14 -0800

    32 block sweet spot still in this

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -41,8 +41,8 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
           double cij = C[i + j * lda];
 
           for (int k = 0; k < K; k+=2) {
-              cij += &a[i+k*lda] * B[k+j*lda];
-              cij += &a[(i+1)+(k+1)*lda] * B[(k+1)+(j+1)*lda];
+              cij += a[i+k*lda] * B[k+j*lda];
+              cij += a[(i+1)+(k+1)*lda] * B[(k+1)+(j+1)*lda];
           }
           C[i + j * lda] = cij;
 
