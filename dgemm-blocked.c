229faa84d539fe885e94f8a78cbe907071e4e060
hunter-graves <server.hunter@gmail.com>
2018-03-09 07:07:12 -0800

    32 block sweet spot? unaligned with slowblock change

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -28,17 +28,17 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
   /* For each row i of A */
-  for (int i = 0; i < M; i+=2)
-    /* For each column j of B */ 
-    for (int j = 0; j < N; j+=2)
-    {
-      /* Compute C(i,j) */
-      double cij = C[i+j*lda];
-      for (int k = 0; k < K; k+=2)
-	cij += A[i+k*lda] * B[k+j*lda];
-      C[i+j*lda] = cij;
-    }
-}
+  for (int i = 0; i < M; i+=2) {
+      /* For each column j of B */
+      for (int j = 0; j < N; j += 2) {
+          /* Compute C(i,j) */
+          double cij = C[i + j * lda];
+          for (int k = 0; k < K; k += 2)
+              cij += A[i + k * lda] * B[k + j * lda];
+          C[i + j * lda] = cij;
+      }
+  }
+  }
 
 
 
