8122a2f3806b93ae987ed7a553d01b4c6d946957
hunter-graves <server.hunter@gmail.com>
2018-03-09 05:42:42 -0800

    32block sweet spot

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -269,17 +269,16 @@ void square_dgemm (int lda, double* A, double* B, double* C)
 
 	/* Perform individual block dgemm */
 	//do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
-if ((M%BLOCK_SIZE != 0) || (N%BLOCK_SIZE !=0) || (K%BLOCK_SIZE!=0))
-{
+//if ((M%BLOCK_SIZE != 0) || (N%BLOCK_SIZE !=0) || (K%BLOCK_SIZE!=0))
+//{
 
     //int M = min (BLOCK_SIZE/2, lda-i);
     //int N = min (BLOCK_SIZE/2, lda-j);
     //int K = min (BLOCK_SIZE/2, lda-k);
     //int baby1 = lda-k;
-    void baby_block (int baby1, int lda, int M, int N, int K, double* A, double* B, double* C);
+   // void baby_block (int baby1, int lda, int M, int N, int K, double* A, double* B, double* C);
 
-
-}
+//}
 
 
 
