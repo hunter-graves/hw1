d2503c08e3ba6f481aeae550eaff3f621f3bb39b
hunter-graves <server.hunter@gmail.com>
2018-03-09 06:46:02 -0800

    8 blocks?

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -17,7 +17,7 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #include <emmintrin.h>
 #include <immintrin.h>
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 32
+#define BLOCK_SIZE 8
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
@@ -48,14 +48,7 @@ void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* 
 {
     static double a[BLOCK_SIZE*BLOCK_SIZE] __attribute__ ((aligned (32)));
     static double temp[4] __attribute__ ((aligned (32)));
-    /*__m128d vecA1;
-    __m128d vecB1;
-    __m128d vecC1;
-    __m128d vecA2;
-    __m128d vecB2;
-    __m128d vecC2;
-    __m128d vecCtmp;
-     */
+
 
     __m256d vec1A;
     __m256d vec1B;
@@ -66,14 +59,6 @@ void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* 
     __m256d vecCtmp;
     __m256d vecCtmp2;
 
-
-
-
-    //double a1,a2,a3,a4,b1,b2,b3,b4,c1,c2,c3,c4;
-//make a local aligned copy of A's block
-    //for( int j = 0; j < K; j++ )
-      //  for( int i = 0; i < M; i++ )
-        //a[i+j*BLOCK_SIZE] = A[i+j*lda];
 
 
     for( int i = 0; i < M; i++ )
@@ -106,56 +91,6 @@ void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* 
                 cij += temp[3];
 
 
-
-
-
-
-
-
-
-/*
-                a1 = a[i+k*BLOCK_SIZE];
-                a2 = a[i+(k+1)*BLOCK_SIZE];
-                a3 = a[i+(k+2)*BLOCK_SIZE];
-                a4 = a[i+(k+3)*BLOCK_SIZE];
-                b1 = B[k+j*lda];
-                b2 = B[(k+1)+j*lda];
-                b3 = B[(k+2)+j*lda];
-                b4 = B[(k+3)+j*lda];
-                c1 = a1 * b1;
-                c2 = a2 * b2;
-                c3 = a3 * b3;
-                c4 = a4 * b4;
-                cij += c1;
-                cij += c2;
-                cij += c3;
-                cij += c4;
-*/
-
-                /*vecA1 = _mm_load_pd (&a[k+i*BLOCK_SIZE]);
-                vecA2 = _mm_load_pd (&a[(k+2)+i*BLOCK_SIZE]);
-                vecB1 = _mm_loadu_pd (&B[k+j*lda]);
-                vecB2 = _mm_loadu_pd (&B[(k+2)+j*lda]);
-                vecC1 = _mm_mul_pd(vecA1, vecB1);
-                vecC2 = _mm_mul_pd(vecA2, vecB2);
-                vecCtmp = _mm_add_pd(vecC1, vecC2);
-                _mm_storeu_pd(&temp[0], vecCtmp);
-                cij += temp[0];
-                cij += temp[1];
-*/
-
-
-
-
-
-                //cij += a[i+k*BLOCK_SIZE] * B[k+j*lda];
-                //cij += a[i+(k+1)*BLOCK_SIZE] * B[(k+1)+j*lda];
-
-
-
-
-
-                //cij += a[i+k*BLOCK_SIZE] * B[k+j*lda];
             }
             C[i + j * lda] = cij;
 
@@ -165,88 +100,6 @@ void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-void baby_block (int baby1, int lda, int M, int N, int K, double* A, double* B, double* C)
-{
-    int babyBlock = baby1;
-    double a[babyBlock*babyBlock] __attribute__ ((aligned (16)));
-    static double temp[4] __attribute__ ((aligned (32)));
-
-
-    //double a1,a2,a3,a4,b1,b2,b3,b4,c1,c2,c3,c4;
-
-    __m256d vec1A;
-    __m256d vec1B;
-    __m256d vec1C;
-    __m256d vec2A;
-    __m256d vec2B;
-    __m256d vec2C;
-    __m256d vecCtmp;
-    __m256d vecCtmp2;
-
-    for( int i = 0; i < M; i++ )
-        for( int j = 0; j < K; j++ )
-            a[j+i*babyBlock] = A[i+j*lda];
-
-
-    /* For each row i of A */
-    for (int i = 0; i < M; i+=64)
-/* For each column j of B */
-        for (int j = 0; j < N; j+=64) {
-/* Compute C(i,j) */
-
-            double cij = C[i+j*lda];
-            for (int k = 0; k < K; k+=8) {
-/*
-                a1 = a[i + k * babyBlock];
-                a2 = a[i + (k + 1) * babyBlock];
-                a3 = a[i + (k + 2) * babyBlock];
-                a4 = a[i + (k + 3) * babyBlock];
-                b1 = B[k + j * lda];
-                b2 = B[(k + 1) + j * lda];
-                b3 = B[(k + 2) + j * lda];
-                b4 = B[(k + 3) + j * lda];
-                c1 = a1 * b1;
-                c2 = a2 * b2;
-                c3 = a3 * b3;
-                c4 = a4 * b4;
-                cij += c1;
-                cij += c2;
-                cij += c3;
-                cij += c4;
-                */
-
-
-                vec1A = _mm256_load_pd (&a[k+i*BLOCK_SIZE]);
-                vec1B = _mm256_loadu_pd (&B[k+j*lda]);
-                vec2A = _mm256_load_pd (&a[k+4+i*BLOCK_SIZE]);
-                vec2B = _mm256_loadu_pd (&B[k+4+j*lda]);
-                vec1C = _mm256_mul_pd(vec1A, vec1B);
-                vec2C = _mm256_mul_pd(vec2A, vec2B);
-                vecCtmp = _mm256_add_pd(vec1C,vec2C);
-                _mm256_store_pd(&temp[0], vecCtmp);
-
-
-                cij += temp[0];
-                cij += temp[1];
-                cij += temp[2];
-                cij += temp[3];
-
-            }
-        }
-
-
-
-}
 
 
 /* This routine performs a dgemm operation
@@ -267,20 +120,6 @@ void square_dgemm (int lda, double* A, double* B, double* C)
 	int N = min (BLOCK_SIZE, lda-j);
 	int K = min (BLOCK_SIZE, lda-k);
 
-	/* Perform individual block dgemm */
-	//do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
-//if ((M%BLOCK_SIZE != 0) || (N%BLOCK_SIZE !=0) || (K%BLOCK_SIZE!=0))
-//{
-
-    //int M = min (BLOCK_SIZE/2, lda-i);
-    //int N = min (BLOCK_SIZE/2, lda-j);
-    //int K = min (BLOCK_SIZE/2, lda-k);
-    //int baby1 = lda-k;
-  //  void baby_block (int baby1, int lda, int M, int N, int K, double* A, double* B, double* C);
-
-//}
-
-
 
           if((M % BLOCK_SIZE == 0) && (N % BLOCK_SIZE == 0) && (K % BLOCK_SIZE == 0))
           {
@@ -288,15 +127,6 @@ void square_dgemm (int lda, double* A, double* B, double* C)
           }else{
               do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
           }
-
-
-
-
-
-
-
-
-
 
 
 
