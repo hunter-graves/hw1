b9d062f8fbfc9b2ab303a286d28fd529b1f224b9
hunter-graves <server.hunter@gmail.com>
2018-03-09 21:05:10 -0800

    leeerrroooyyy jeeennnnkinsss

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -17,7 +17,7 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #include <emmintrin.h>
 #include <immintrin.h>
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 96
+#define BLOCK_SIZE 34
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
